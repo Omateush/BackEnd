5add5a8b936ecd2df581a0a3f48b0aadb223138a
Mateus <145764702+Omateush@users.noreply.github.com>
2024-02-08 18:25:01 +0000

    Add files via upload

---
+++ TPSI_DWBE_24/Ficha1/app.js
@@ -0,0 +1,37 @@
+// IMPLEMENTAÇÃO
+function calculateGrade(gradeP, gradeT){
+    var mean = gradeT * 0.4 + gradeP * 0.6;
+    console.log("Your average is: " + mean );
+
+    if (mean < 9.5)
+        console.log("you failed");
+    else
+        console.log("you passed!");
+    
+}
+
+// INVOCAÇÃO
+
+calculateGrade(14, 9);
+
+function month(MonthNumber){
+    switch (MonthNumber) {
+        case 1:
+        janeiro
+            break;
+    
+        default:
+            console.log("Valor inválido!");
+            break;
+    }
+    switch (MonthNumber) {
+        case 2:
+        Fevereiro            
+            break;
+    
+        default:
+            console.log("Valor Inválido!");
+            break;
+    }
+
+}
